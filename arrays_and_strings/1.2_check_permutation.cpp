916fd3638731806da99f92e48033010c2983e5c8
adrientremblay <adrien@adrientremblay.com>
2021-11-15 21:04:15 -0500

    Add a char count array solution for 1.2

--- arrays_and_strings/1.2_check_permutation.cpp
+++ arrays_and_strings/1.2_check_permutation.cpp
@@ -19,11 +19,32 @@ Solutions:
 **/
 
 /**
+Sort Solution
+Time: O(N*logN)
+Space: O(1)
+**/
+bool sort_both(string a, string b) {
+    if (a.length() != b.length())
+        return false;
+
+    vector<char> va(a.begin(), a.end());
+    sort(va.begin(), va.end());
+
+    vector<char> vb(b.begin(), b.end());
+    sort(vb.begin(), vb.end());
+
+    return va == vb;
+}
+
+/**
 Hash Map Solution
 Time: O(N)
 Space: O(1)
 **/
 bool hash_map(string a, string b) {
+    if (a.length() != b.length())
+        return false;
+
     unordered_map<char, int> map;
     
     for (char c : a)
@@ -45,14 +66,25 @@ bool hash_map(string a, string b) {
     return true;
 }
 
-bool sort_both(string a, string b) {
-    vector<char> va(a.begin(), a.end());
-    sort(va.begin(), va.end());
+/**
+Char Count Array Solution
+Time: O(N)
+Space: O(1)
+**/
+bool count_array(string a, string b) {
+    if (a.length() != b.length())
+        return false;
+    
+    int char_count[128] = { 0 };
 
-    vector<char> vb(b.begin(), b.end());
-    sort(vb.begin(), vb.end());
+    for (char c : a)
+        char_count[(int) c]++;
 
-    return va == vb;
+    for (char c : b) 
+        if (--char_count[(int) c] < 0)
+            return false;
+
+    return true ;
 }
 
 int main() {
@@ -60,7 +92,8 @@ int main() {
     
     for (auto& test : tests) {
         cout << test[0] << " " << test[1] << endl;
-        cout << "HASH MAP SOLUTION: " <<  hash_map(test[0], test[1]) << endl;
         cout << "SORT SOLUTION: " << hash_map(test[0], test[1]) << endl;
+        cout << "HASH MAP SOLUTION: " <<  hash_map(test[0], test[1]) << endl;
+        cout << "CHAR COUNT ARRAY: " <<  count_array(test[0], test[1]) << endl;
     }
 }
