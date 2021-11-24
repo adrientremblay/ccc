5f8ace3075fce33e0dadfb7707182b36838c0437
adrientremblay <adrien@adrientremblay.com>
2021-11-24 08:14:25 -0500

    Create 1.4PalindromePermutation.cpp

---
+++ arrays_and_strings/1.4PalindromePermutation.cpp
@@ -0,0 +1,17 @@
+/**
+Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of the letters. The palindrome does not need to be limited to just dictionary words.
+
+notes:
+- ignore spaces
+
+solutions:
+- brute force: generate every single permutation, check each one if it's a permutation (not coding this lol)
+- hash map, check every char has an even count, except at most one char
+- sort and check that every num has even count, except at most one char
+
+Ex:
+Tact Coa
+**/
+
+bool hashmap(string s) {
+}
